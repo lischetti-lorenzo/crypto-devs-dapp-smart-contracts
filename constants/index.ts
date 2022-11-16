7b719ee43018957682098f72a798b77a6b30ff4b
Lorenzo Lischetti <lischetti.lorenzo@gmail.com>
2022-11-16 01:23:05 -0300

    Removed rinkeby contracts

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
 export const WHITELIST_CONTRACT_ADDRESS = "0xdF563EF1E141865e7cc32B3AC13e24A04eAF2735";
-export const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x9566D74F3D9d26394a8908D19baB89c68432CAaf";
+export const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x992B9E797fe1931b4bf6Cb142AD2d38500131C1d";
 // URL to extract Metadata for a Crypto Dev NFT
 export const METADATA_URL = "https://crypto-devs-dapp-client.vercel.app/api/tokens/";
